792b5d36aa27a4664ada10f6bc63a6f7bfc1bd44
Earvin-Matsumoto <e.matsumoto@earvin-s.com>
2026-05-05 15:03:42 +0900

    Add files via upload

---
+++ middleware.js
@@ -0,0 +1,23 @@
+export function middleware(request) {
+  const basicAuth = request.headers.get('authorization')
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1]
+    const [user, pwd] = atob(authValue).split(':')
+
+    if (user === 'xacti' && pwd === 'xacti2026') {
+      return
+    }
+  }
+
+  return new Response('このページにアクセスするには認証が必要です。', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Xacti Private", charset="UTF-8"',
+    },
+  })
+}
+
+export const config = {
+  matcher: '/:path*',
+}
